b98b470978ef829901d9d9bcc8ba7d158fe3e1a7
toniname <60040991+toniname@users.noreply.github.com>
2024-05-16 22:18:49 +0300

    Initial commit

---
+++ src/main/java/com/example/app/Note.java
@@ -0,0 +1,35 @@
+package com.example.app;
+import lombok.Data;
+
+@Data
+public class Note {
+    private Long id;
+    private String title;
+    private String content;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}

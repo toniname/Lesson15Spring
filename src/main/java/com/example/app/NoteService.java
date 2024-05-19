e7bc82a73bfaa3bc6743114b771a8943c1aed873
toniname <60040991+toniname@users.noreply.github.com>
2024-05-19 20:19:21 +0300

    init commit_3

--- src/main/java/com/example/app/NoteService.java
+++ src/main/java/com/example/app/NoteService.java
@@ -1,5 +1,6 @@
 package com.example.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,41 +10,33 @@ import java.util.Map;
 
 @Service
 public class NoteService {
-    private final Map<Long, Note> notes = new HashMap<>();
-    private long nextId = 1;
-    private static final String MY_CONSTANT1 = "Note with id ";
-    private static final String MY_CONSTANT2 = " not found";
+
+    @Autowired
+    NoteRepository noteRepository;
 
     public List<Note> listAll() {
-        return new ArrayList<>(notes.values());
+        return noteRepository.findAll();
     }
 
     public Note add(Note note) {
-        note.setId(nextId++);
-        notes.put(note.getId(), note);
-        return note;
+        return noteRepository.save(note);
     }
 
     public void deleteById(long id) {
-        if (!notes.containsKey(id)) {
-            throw new IllegalArgumentException(MY_CONSTANT1 + id + MY_CONSTANT2);
-        }
-        notes.remove(id);
+        noteRepository.deleteById(id);
     }
 
     public void update(Note note) {
-        if (!notes.containsKey(note.getId())) {
-            throw new IllegalArgumentException(MY_CONSTANT1 + note.getId() + MY_CONSTANT2);
+        if (noteRepository.existsById(note.getId())) {
+            noteRepository.save(note);
+        }else {
+            throw new RuntimeException("Note not found with id " + note.getId());
         }
-        notes.put(note.getId(), note);
+
     }
 
     public Note getById(long id) {
-        Note note = notes.get(id);
-        if (note == null) {
-            throw new IllegalArgumentException(MY_CONSTANT1 + id + MY_CONSTANT2);
-        }
-        return note;
+        return noteRepository.findById(id).orElseThrow(() -> new RuntimeException("Note not found with id " + id));
     }
 }
 

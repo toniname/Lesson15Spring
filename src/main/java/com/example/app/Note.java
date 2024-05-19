f57880044d5308685557bd4d83b820fa9464beb8
toniname <60040991+toniname@users.noreply.github.com>
2024-05-19 11:43:01 +0300

    init commit_3

--- src/main/java/com/example/app/Note.java
+++ src/main/java/com/example/app/Note.java
@@ -1,8 +1,15 @@
 package com.example.app;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+@Entity
 @Data
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
---
+++ src/main/java/com/example/app/NoteRepository.java
@@ -0,0 +1,8 @@
+package com.example.app;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}

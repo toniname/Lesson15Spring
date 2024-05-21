ce3dcd3176cb4eb6abd9f745a9b4ffb00a603130
toniname <60040991+toniname@users.noreply.github.com>
2024-05-21 22:44:30 +0300

    init commit_1

---
+++ src/main/java/com/example/app/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .defaultSuccessUrl("/", true)
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        if (!userDetailsManager.userExists("user")) {
+            userDetailsManager.createUser(User.withUsername("user")
+                    .password("{noop}jdbcDefault")
+                    .roles("USER")
+                    .build());
+        }
+        return userDetailsManager;
+    }
+}

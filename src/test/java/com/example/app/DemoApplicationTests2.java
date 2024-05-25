73644d7b1ce9d1adc77ab087c4a8cd934cc906cc
toniname <60040991+toniname@users.noreply.github.com>
2024-05-25 17:57:44 +0300

    change test2

--- src/test/java/com/example/app/DemoApplicationTests2.java
+++ src/test/java/com/example/app/DemoApplicationTests2.java
@@ -3,6 +3,7 @@ package com.example.app;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 @WebMvcTest(NoteController.class)
 class DemoApplicationTests2 {
@@ -51,7 +51,8 @@ class DemoApplicationTests2 {
     @Test
     public void testDeleteNote() throws Exception {
         mockMvc.perform(post("/note/delete")
-                        .param("id", "1"))
+                        .param("id", "1")
+                        .with(csrf())) // Додаємо CSRF-токен до запиту
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/note/list"));
 
@@ -75,7 +76,8 @@ class DemoApplicationTests2 {
         note.setContent("This is an updated note.");
 
         mockMvc.perform(post("/note/edit")
-                        .flashAttr("note", note))
+                        .flashAttr("note", note)
+                        .with(csrf())) // Додаємо CSRF-токен до запиту
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/note/list"));
 
